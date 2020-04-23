291fa2307ae13325df1b068246556561c416b683
karl <kwkroll.32@gmail.com>
2020-04-23 06:55:29 -0400

    added gui nav from contra. added tapdance tab. reset button on both halves.

--- keyboards/ergodox_infinity/keymaps/karl/keymap.c
+++ keyboards/ergodox_infinity/keymaps/karl/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
         KC_EQL,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5, KC_ESC,
         KC_BSLS, KC_Q,   KC_W,    KC_E,    KC_R,    KC_T, TG(_NAV),
-        KC_TAB,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,
+        TD(NAV),  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, MO(_NAV),
         CTL_T(KC_HOME), KC_LGUI, KC_LALT, LGUI(KC_LSFT), TD(LOWER),
                                                       KC_LCTL, KC_LALT,
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
              MO(_NAV),  KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
                               TD(RAISE), TD(NAV), KC_RALT,  KC_RGUI, CTL_T(KC_END),
-             KC_RALT, KC_RCTL,
+             KC_RALT, TD(MENU),
              KC_PGUP,
              KC_PGDN, KC_ENT, KC_SPC
     ),
@@ -135,7 +135,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *        |     +-----+-----+-----+-----+-----+-------+
  *        +-----+ Left| Down|  Up |Right|pg up|       |
  *        |     +-----+-----+-----+-----+-----+-------+
- *        |     |     |     |     |     |pg dn|       |
+ *        |     | GC_L|GC_D |GC_U |GC_D |pg dn|       |
  *        +-----+-----+-----+-----+-----+-----+-----+-+
  *                    |     |     |     |     |     |
  *    +-----+-----+   +-----+-----+-----+-----+-----+
@@ -149,7 +149,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
        KC_TRNS, RGUI(KC_LEFT), RGUI(KC_DOWN),RGUI(KC_UP), RGUI(KC_RGHT), KC_TRNS, KC_TRNS,
                 KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_PGUP, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_TRNS,
+       KC_TRNS, RCTL(RGUI(KC_LEFT)), RCTL(RGUI(KC_DOWN)), RCTL(RGUI(KC_RGHT)), RCTL(RGUI(KC_UP)), KC_PGDN, KC_TRNS,
                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
@@ -185,7 +185,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   KC_TRNS, KC_TRNS, KC_TRNS,
 /* right hand
  *        +-----+-----+-----+-----+-----+-----+-------+
- *        |     |     |     |     |     |     |       |
+ *        |FLASH|     |     |     |     |     |       |
  *        +-----+-----+-----+-----+-----+-----+-------+
  *        |     |  ^  |  &  |  *  |  (  |  )  |       |
  *        |     +-----+-----+-----+-----+-----+-------+
@@ -202,7 +202,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *    |     |     |     |
  *    +-----+-----+-----+
  */
-       KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS,     KC_TRNS,        KC_TRNS,     KC_TRNS,
+       RESET,    KC_TRNS, KC_TRNS,    KC_TRNS,     KC_TRNS,        KC_TRNS,     KC_TRNS,
        KC_TRNS,  KC_CIRC, KC_AMPR,    KC_ASTR,     KC_LPRN,        KC_RPRN,     KC_TRNS,
                  KC_F6,   KC_UNDS,    KC_PLUS,     KC_LCBR,        KC_RCBR,     KC_PIPE,
        KC_TRNS,  KC_F12,  KC_TRNS,    KC_TRNS,     KC_TRNS,        KC_TRNS,     KC_TRNS,
@@ -242,7 +242,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   KC_TRNS, KC_TRNS, KC_TRNS,
 /* right hand
  *        +-----+-----+-----+-----+-----+-----+-------+
- *        |     |     |     |     |     |     |       |
+ *        |FLASH|     |     |     |     |     |       |
  *        +-----+-----+-----+-----+-----+-----+-------+
  *        |     |  6  |  7  |  8  |  9  |  0  |       |
  *        |     +-----+-----+-----+-----+-----+-------+
@@ -259,7 +259,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *    |     |     |     |
  *    +-----+-----+-----+
  */
-       KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS,     KC_TRNS,        KC_TRNS,     KC_TRNS,
+       RESET,    KC_TRNS, KC_TRNS,    KC_TRNS,     KC_TRNS,        KC_TRNS,     KC_TRNS,
        KC_TRNS,  KC_6,    KC_7,       KC_8,        KC_9,           KC_0,        KC_TRNS,
                  KC_F6,   KC_MINS,    KC_EQL,      KC_LBRC,        KC_RBRC,     KC_BSLS,
        KC_TRNS,  KC_F12,  KC_TRNS,    KC_TRNS,     KC_TRNS,        KC_TRNS,     KC_TRNS,
