840c974e49077fde908a39aa4f2e07c3eb881bed
Karl Kroll <kwkroll.32@gmail.com>
2023-09-17 11:19:54 -0400

    add td and reset key to 7v

--- keyboards/wilba_tech/wt75_c/keymaps/karl/keymap.c
+++ keyboards/wilba_tech/wt75_c/keymaps/karl/keymap.c
@@ -1,22 +1,49 @@
 #include QMK_KEYBOARD_H
 
+//Tap Dance Declarations
+enum {
+  MENU,
+};
+
+void td_menu_selct (tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1 && !state->pressed) {
+    SEND_STRING(SS_TAP(X_APPLICATION));
+    reset_tap_dance(state);
+  } else if (state->count == 2 && !state->pressed) {
+    SEND_STRING(SS_TAP(X_HOME) SS_DOWN(X_LEFT_SHIFT) SS_TAP(X_END) SS_UP(X_LEFT_SHIFT));
+//    SEND_STRING(SS_TAP(X_HOME) SS_DOWN(X_LEFT_SHIFT) SS_TAP(X_END));
+    reset_tap_dance(state);
+  } else {
+    // hard reset
+    state->count = 0;
+    state->interrupted = false;
+    state->finished = false;
+  }
+}
+
+//Tap Dance Definitions
+tap_dance_action_t tap_dance_actions[] = {
+  [MENU]  = ACTION_TAP_DANCE_FN(td_menu_selct),
+};
+// end TD
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	// Default layer
 	// NOTE: use bootmagic for reset, i.e. hold esc while plugging in to enter dfu
 	[0] = LAYOUT_all(
-		KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,
+		KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  TD(MENU),
 		KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, KC_DEL,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
-		KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,			RGUI_T(KC_PGDN),
+		KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,			RGUI_T(KC_PGDN),
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   KC_UP,
-		CTL_T(KC_HOME), KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RGUI, CTL_T(KC_END),            KC_LEFT, KC_DOWN, KC_RGHT),
+		CTL_T(KC_HOME), MO(1), KC_LALT,                            KC_SPC,                             KC_RGUI, CTL_T(KC_END),            KC_LEFT, KC_DOWN, KC_RGHT),
 
 	// Fn1 Layer
 	[1] = LAYOUT_all(
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS),
